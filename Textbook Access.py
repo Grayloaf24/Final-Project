4ed82da81c32d73b359486385a7909ff632c4fe4
Grayloaf24 <grayloaf24@gmail.com>
2022-12-07 16:59:01 -0600

    Started coding

--- Textbook Access.py
+++ Textbook Access.py
@@ -2,3 +2,16 @@
 #Date: December 7, 2022
 #Program Title: Textbook Access
 
+import sqlite3 as lite
+import sys
+
+conn = lite.connect('textbooks.db')
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM author")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    conn.close()
